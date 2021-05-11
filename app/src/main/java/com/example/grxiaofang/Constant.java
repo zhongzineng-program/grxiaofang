ad319006727d72e81ed86156cbd93bc2f81e3bc5
zhongzineng-program <84001177+zhongzineng-program@users.noreply.github.com>
2021-05-12 00:29:54 +0800

    Update Constant.java

--- app/src/main/java/com/example/grxiaofang/Constant.java
+++ app/src/main/java/com/example/grxiaofang/Constant.java
@@ -3,5 +3,5 @@ package com.example.grxiaofang;
 class Constant {
     //Mysql数据库网络配置
     public static final String FORNAME = "com.mysql.jdbc.Driver";
-    public static final String MYSQLBASE_URL = "jdbc:mysql://8.133.158.95:3306/grxiaofang?useSSL=false&serverTimezone=UTC&characterEncoding=utf8";
+    public static final String MYSQLBASE_URL = "jdbc:mysql://这是秘密秘密秘密?useSSL=false&serverTimezone=UTC&characterEncoding=utf8";
 }
